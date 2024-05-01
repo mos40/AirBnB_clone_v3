b8563be45441ef76a8cbcd5f8c0330069d40964e
Moses <mosesmkhonza@outlook.com>
2024-05-01 11:24:42 -0700

    E402 module level import not at top of file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,6 @@
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 """import storage engine and classes"""
 from models import storage
 from models.state import State
@@ -15,5 +18,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
